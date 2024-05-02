82379dba01063beba96efff7240825a247d46b91
Pauldbg <p.duboisgance@gmail.com>
2024-05-02 17:56:08 +0200

    push frontend part project

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,31 @@
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.splitBackground}>
+      <div className={styles.leftHome}>
+        <img className={styles.largeLogo}src="/logo.png" alt="largeLogo"/>
+      </div>
+      <div className={styles.rightHome}>
+        <img className={styles.logo} src={'/logo.png'} alt="logo"/>
+        <h1 className={styles.title}>See what's happening</h1>
+        <h2 className={styles.subtitle}>Join Hackatweet Today.</h2>
+        <div className={styles.buttons}>
+          <button
+            className={styles.signupButton}
+            onClick={() => handleLogout()}
+          >
+            Sign up
+          </button>
+          <p className={styles.accountTitle}>Already have an account ?</p>
+          <button
+            className={styles.signinButton}
+            onClick={() => handleLogout()}
+          >
+            Sign in
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
